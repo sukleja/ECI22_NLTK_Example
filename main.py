70d0a8c8468307b1b3b2dcb6aaf4d9721987f06a
sebastian.ukleja <sebastian.ukleja@fh-campuswien.ac.at>
2021-06-01 18:24:55 +0200

    minor formatting changes

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import nltk
 from nltk.corpus import wordnet
 from nltk.corpus import names
+
 nltk.download('wordnet')
 nltk.download('names')
 
 synonyms = []
 antonyms = []
-word="love"
+word = "love"
 
-syns=wordnet.synsets(word)
+# loads the cognitive synonyms for the word from wordnet
+syns = wordnet.synsets(word)
 
 for syn in syns:
     for l in syn.lemmas():
@@ -16,12 +18,12 @@ for syn in syns:
         if l.antonyms():
             antonyms.append(l.antonyms()[0].name())
 
-print("The definition of: ",word," is: ",syns[0].definition())
-print("Example of usage in an sentence: ",syns[0].examples())
-print("Synonyms of:", word,": ",set(synonyms))
-print("Antonyms of:", word,": ",set(antonyms))
+print("The definition of: ", word, " is: ", syns[0].definition())
+print("Example of usage in an sentence: ", syns[0].examples())
+print("Synonyms of:", word, ": ", set(synonyms))
+print("Antonyms of:", word, ": ", set(antonyms))
 
-female_names=names.words('female.txt')
-male_names=names.words('male.txt')
+female_names = names.words('female.txt')
+male_names = names.words('male.txt')
 print(female_names)
 print(male_names)
