75a9848e1884937f745afd7a368e5de3168e7118
sebastian.ukleja <sebastian.ukleja@fh-campuswien.ac.at>
2022-05-17 10:38:27 +0200

    added tag checking and comments

--- main.py
+++ main.py
@@ -1,29 +1,34 @@
+'''Some examples how to use NLTK with the Wordnet synsets'''
 import nltk
 import example
 from nltk.corpus import wordnet
 from nltk.corpus import names
 
-'''Downloading either with interactive downloader or directly by stating the module'''
-#nltk.download()
-#nltk.download('wordnet')
+'''Downloading either with interactive downloader or directly by stating the module
+has to be only done once and can be commented out after the download'''
+#nltk.download() #using the interactive downloader
+#nltk.download('wordnet') #downloading the specific package without interactive downloader
 #nltk.download('names')
 
+'''preparing variables'''
 synonyms = []
 antonyms = []
 examples = []
+tag = ''
 word = "love"
 
-# loads the cognitive synonyms for the word from wordnet
+'''loads the cognitive synonyms for the word from the lexical database wordnet (called synsets in Wordnet)'''
 syns = wordnet.synsets(word)
 
-#appending synonyms and antonyms to list
+'''appending synonyms and antonyms to list
+lemmas are Wordnets canonical forms of words and represent the different synonyms and antonyms in this case'''
 for syn in syns:
     for l in syn.lemmas():
         synonyms.append(l.name())
         if l.antonyms():
             antonyms.append(l.antonyms()[0].name())
 
-#handling empty lists and loading example uses of the word
+'''handling empty lists and loading example uses of the word'''
 if len(antonyms) == 0:
     antonyms.append("No antonyms found")
 
@@ -32,17 +37,30 @@ if not syns[0].examples():
 else:
     for ex in syns[0].examples():
         examples.append(ex)
-#####################
 
-print("The definition of: ", word, " is: ", syns[0].definition())
-print("Example of usage in an sentence: ", examples)
-print("Synonyms of:", word, ": ", set(synonyms))
-print("Antonyms of:", word, ": ", set(antonyms))
+
+'''Checking part of speach'''
+if syns[0].pos() == 'n':
+    tag = 'noun'
+elif syns[0].pos() == 'a':
+    tag = 'adjective'
+elif syns[0].pos() == 'v':
+    tag = 'verb'
+elif syns[0].pos() == 'r':
+    tag = 'adverb'
+
+'''Printing out our information'''
+print("The definition of: {} ({}) is: {}".format(word,tag,syns[0].definition()))
+print("Examples of usage in an sentence: ", examples)
+print("Synonyms of {} : {}".format(word, set(synonyms)))
+print("Antonyms of {} : {}".format(word, set(antonyms)))
 print('')
 
-#loading name files and printing out the first 6 entries of the lists
+
+'''Example for using the names corpus'''
 female_names = names.words('female.txt')
 male_names = names.words('male.txt')
+print("The first six female and male names in the names corpus:")
 print(female_names[0:6])
 print(male_names[0:6])
-#example.print_out()
+
